35fd364d91a5c4e3353f0110a64df49d02efad5e
KevGomez10 <kevingo2806@gmail.com>
2025-11-07 14:22:49 -0500

    añadi un nuevo ejercicio :)

---
+++ convertir_unidades.py
@@ -0,0 +1,20 @@
+print("Bienvenido al conversor de unidades")
+print('Por favor ingresa una opcion y lo convertire tu valor de : ' , '1.Celsius a Fahremheit' , '2.Kilometros a millas' , '3.Metros a pies' , sep = '\n\n')
+opcion = int(input("Ingresa la opcion que deseas  :) "))
+valor = int(input("Ingresa el valor que quieres convertir : "))
+
+
+if (opcion == 1  ):
+    Fahrenheit = (valor * 9/5)+32 
+    print("Digitaste" , valor , "Que en Fahremheit serian : " , Fahrenheit)
+
+elif (opcion == 2):
+     Millas = valor * 0.621371
+     print("Digitaste" , valor , "KM que en millas serian " , Millas)
+
+elif (opcion == 3):
+    Pies = valor * 3.28084
+
+else:
+    print("Ingrese una opcion valida por favor : ") 
+    
